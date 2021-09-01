3f76282e41d47b5a540b60b18a6dd53b9c338c9b
vtumanov782 <vtumanov782@gmail.com>
2021-09-01 20:58:28 +0300

    graph typing

--- src/graph/types.ts
+++ src/graph/types.ts
@@ -2,6 +2,17 @@ export interface GraphEdge<E extends unknown> {
   value: E;
   next: Array<GraphEdge<E>>;
 }
+export interface Iterable<E extends unknown> {
+  [Symbol.iterator]: () => {
+    next: () => {
+      value: E,
+      done: false
+    } | {
+      done: true
+    };
+  }
+}
 
 export type Create = <E extends unknown>() => GraphEdge<E>;
-export type connect = <E extends unknown>(from: GraphEdge<E>, to: GraphEdge<E>) => void;
+export type Connect = <E extends unknown>(from: GraphEdge<E>, to: GraphEdge<E>) => void;
+export type Traverse = <E extends unknown>(start: GraphEdge<E>) => Iterable<E>;
