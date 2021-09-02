8ce8e737bd577130f13d38b71d34ce597007bc0d
vtumanov782 <vtumanov782@gmail.com>
2021-09-02 14:15:42 +0300

    deep & breadth graph traversial

--- src/graph/implementation.ts
+++ src/graph/implementation.ts
@@ -10,5 +10,72 @@ export const connect: Connect = <E extends unknown>(from: GraphEdge<E>, to: Grap
   to.next.add(from);
 };
 
-export const deepTraverse: Traverse = <E extends unknown>(start: GraphEdge<E>) => null;
-export const breadthTraverse: Traverse = <E extends unknown>(start: GraphEdge<E>) => null;
+export const deepTraverse: Traverse = <E extends unknown>(start: GraphEdge<E>) => ({
+  [Symbol.iterator]: () => {
+    const visited = new Set<GraphEdge<E>>();
+    const stack = [] as Array<GraphEdge<E>>;
+
+    stack.unshift(start);
+    visited.add(start);
+
+    return {
+      next: () => {
+        if (stack.length === 0) {
+          return {
+            done: true
+          };
+        }
+
+        const result = {
+          done: false as const,
+          value: stack[0].value,
+        };
+
+        let nextUnvisited: GraphEdge<E> | undefined;
+        while (nextUnvisited === undefined && stack.length > 0) {
+          nextUnvisited = [...stack[0].next].find(edge => !visited.has(edge));
+          if (!nextUnvisited) stack.shift();
+        }
+        if (nextUnvisited !== undefined) {
+          stack.unshift(nextUnvisited);
+          visited.add(nextUnvisited);
+        }
+
+        return result;
+      }
+    };
+  }
+});
+
+export const breadthTraverse: Traverse = <E extends unknown>(start: GraphEdge<E>) => ({
+  [Symbol.iterator]: () => {
+    const visited = new Set<GraphEdge<E>>();
+    const queue = [] as Array<GraphEdge<E>>;
+    queue.push(start);
+    visited.add(start);
+
+    return {
+      next: () => {
+        if (queue.length === 0) {
+          return {
+            done: true
+          };
+        }
+
+        // enqueue next edges
+        for (const edge of queue[0].next) {
+          if (!visited.has(edge)) {
+            queue.push(edge);
+            visited.add(edge);
+          }
+        }
+
+        return {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          value: queue.shift()!.value,
+          done: false as const
+        }
+      }
+    };
+  }
+});
